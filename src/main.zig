ff18b944a34eadf7da29b0ad6c41c44268bba794
Wannes Vantorre <vantorrewannes@gmail.com>
2025-07-17 15:03:57 +0200

    feature: Cleanup project and fix exports.

--- src/main.zig
+++ src/main.zig
@@ -3,22 +3,4 @@ const lis_lcs = @import("lis_lcs");
 
 pub fn main() !void {
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try lis_lcs.bufferedPrint();
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
 }
--- src/root.zig
+++ src/root.zig
@@ -1,24 +1,7 @@
 const std = @import("std");
-pub const lis = @import("lis.zig");
-pub const lcs = @import("lcs.zig");
-
-pub fn bufferedPrint() !void {
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
-}
+const lis = @import("lis.zig");
+const lcs = @import("lcs.zig");
+pub const longestCommonSubsequence = lcs.longestCommonSubsequence;
 
 test {
     _ = lis;
